8ad315e9131d834ee6b2af786071ac6dca1d61bd
Daniel <dcs98@pitt.edu>
2022-04-15 11:12:52 -0400

    Update config.hpp

--- hw/config.hpp
+++ hw/config.hpp
@@ -5,12 +5,12 @@ namespace CFG
     // note: in_size + 2pad - k must be multiple of stride
     // i.e. a = 0 must hold for the current implementation
 
-    const int in_channels = 3;
-    const int out_channels = 2;
-    const int in_size = 4;
-    const int kernel_size = 4;
-    const int stride = 3;
-    const int pad = 3;
+    const int in_channels = 256;
+    const int out_channels = 128;
+    const int in_size = 100;
+    const int kernel_size = 2;
+    const int stride = 2;
+    const int pad = 0;
     const int a = (in_size + 2*pad - kernel_size) % stride;
     const int test = in_size + 2 *pad - kernel_size;
 
