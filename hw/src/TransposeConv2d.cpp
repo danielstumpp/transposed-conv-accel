097fb4ca60b0717b74a24b469c43f1868b7f1ed6
Daniel <dcs98@pitt.edu>
2022-04-22 17:27:06 -0400

    removed sparsity and validated for v1

--- hw/src/TransposeConv2d.cpp
+++ hw/src/TransposeConv2d.cpp
@@ -70,41 +70,26 @@ void TransposeConv2d_arr(DTYPE *in, DTYPE *bias, DTYPE *kernel, DTYPE *out)
 
     // This first part is where most of the weird stuff happens
     constexpr int inpad = MAX(CFG::kernel_size - CFG::pad - 1, 0);                           
-    constexpr int pin_size = CFG::in_size + (CFG::in_size-1)*(CFG::stride - 1) +  2*inpad;  
-    DTYPE padded_in[CFG::cTile][pin_size][pin_size];
 
-    for (int ct = 0; ct < CFG::in_channels; ct+=CFG::cTile){
-        for (int c = 0; c < CFG::cTile; ++c){
-            for (int i = 0; i < pin_size; ++i){
-                for (int j = 0; j < pin_size; ++j){
-                    padded_in[c][i][j] = 0;
-                }
-            }
-        }
-        
-        for (int c = 0; c < CFG::cTile; ++c){
-            for (int h = 0; h < CFG::in_size; ++h){
-                for (int w = 0; w < CFG::in_size; ++w){
-                    padded_in[c][inpad + h*CFG::stride][inpad + w*CFG::stride] = in[(c+ct)*CFG::in_size*CFG::in_size + h*CFG::in_size + w]; 
-                }
-            }
-        }
+    for (int i = 0; i < CFG::out_channels; ++i){
+        for (int j = 0; j < CFG::in_channels; ++j){
+            for (int h = 0; h < CFG::out_size; ++h){
+                for (int w = 0; w < CFG::out_size; ++w) {
+                    for (int p = 0; p < CFG::kernel_size; ++p) {
+                        for (int q = 0; q < CFG::kernel_size; ++q)
+                        {
+                            int16_t val;
+                            if ((h + p) % CFG::stride == inpad && (w+q) % CFG::stride == inpad){
+                                val = in[j * CFG::in_size * CFG::in_size + (h/CFG::stride) * CFG::in_size + (w/CFG::stride)];
 
-
-        for (int i = 0; i < CFG::out_channels; ++i){
-            for (int j = 0; j < CFG::cTile; ++j){
-                for (int h = 0; h < CFG::out_size; ++h){
-                    for (int w = 0; w < CFG::out_size; ++w) {
-                        for (int p = 0; p < CFG::kernel_size; ++p) {
-                            for (int q = 0; q < CFG::kernel_size; ++q)
-                            {
                                 out[i * CFG::out_size * CFG::out_size + h * CFG::out_size + w] +=
-                                        kernel[(i * CFG::in_channels * CFG::kernel_size * CFG::kernel_size) + ((j + ct) * CFG::kernel_size * CFG::kernel_size) + (p * CFG::kernel_size) + q] * padded_in[j][h + p][w + q];
-                            }
+                                    kernel[(i * CFG::in_channels * CFG::kernel_size * CFG::kernel_size) + (j * CFG::kernel_size * CFG::kernel_size) + (p * CFG::kernel_size) + q] * val;
+                            }                           
                         }
                     }
                 }
             }
         }
     }
+    
 }
