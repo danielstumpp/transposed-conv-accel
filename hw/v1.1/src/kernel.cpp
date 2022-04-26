9bcccb7ad637ac7a1e6e3f2b6559e37dd98b1832
Daniel Stumpp <dcs98@pitt.edu>
2022-04-26 16:01:19 -0400

    fixed timing issues for v1

--- hw/v1.1/src/kernel.cpp
+++ hw/v1.1/src/kernel.cpp
@@ -33,39 +33,44 @@ void TransposeConv2d_kernel(HWTYPE *in, HWTYPE *bias, HWTYPE *kernel, HWTYPE *ou
     #pragma HLS array_partition variable=weights_block dim=3 complete
     #pragma HLS array_partition variable=weights_block dim=4 complete
     #pragma HLS array_partition variable=in_block dim=1 complete
+    #pragma HLS array_partition variable=out_block dim=2 complete
+    #pragma HLS array_partition variable=out_block dim=3 complete
 
     tile_h_loop: for (int ht = 0; ht < CFG::out_size; ht += CFG::osTile){
         tile_w_loop: for (int wt = 0; wt < CFG::out_size; wt += CFG::osTile){
             tile_i_loop: for (int it = 0; it < CFG::out_channels; it += CFG::ocTile){
 
                 // load bias into output block
-                load_bias_h_loop: for (int h = ht; h < MIN(ht + CFG::osTile, CFG::out_size); ++h){
-                    load_bias_w_loop: for (int w = wt; w < MIN(wt + CFG::osTile, CFG::out_size); ++w){
-                        load_bias_i_loop: for (int i = it; i < MIN(it + CFG::ocTile, CFG::out_channels); ++i){
-                            out_block[i - it][h - ht][w - wt] = bias[i];
+                for (int i = 0; i < CFG::ocTile; ++i){
+                    HWTYPE b = bias[it + i];
+                    for (int h = 0; h < CFG::osTile; ++h){
+                        #pragma HLS unroll
+                        for (int w = 0; w < CFG::osTile; ++w){
+                            #pragma HLS unroll
+                            out_block[i][h][w] = b;
                         }
                     }
                 }
 
                 // load kernel weights
-                load_kernel_i_loop: for (int i = it; i < MIN(it + CFG::ocTile, CFG::out_channels); ++i){
+                load_kernel_i_loop: for (int i = 0; i < CFG::ocTile; ++i){
                     load_kernel_j_loop: for (int j = 0; j < CFG::in_channels; ++j){
                         load_kernel_p_loop: for (int p = 0; p < CFG::kernel_size; ++p){
                             load_kernel_q_loop: for (int q = 0; q < CFG::kernel_size; ++q){
-                                weights_block[i - it][j][p][q] = kernel[(i * CFG::in_channels * CFG::kernel_size * CFG::kernel_size) + (j * CFG::kernel_size * CFG::kernel_size) + (p * CFG::kernel_size) + q];
+                                    weights_block[i][j][p][q] = kernel[((i + it) * CFG::in_channels * CFG::kernel_size * CFG::kernel_size) + (j * CFG::kernel_size * CFG::kernel_size) + (p * CFG::kernel_size) + q];
                             }
                         }
                     }
                 }
 
                 // load input features
-                load_in_h_loop: for (int h = ht; h < MIN(ht + CFG::osTile, CFG::out_size); ++h){
-                    load_in_w_loop: for (int w = wt; w < MIN(wt + CFG::osTile, CFG::out_size); ++w){
+                load_in_h_loop: for (int h = 0; h < CFG::osTile; ++h){
+                    load_in_w_loop: for (int w = 0; w < CFG::osTile; ++w){
                         load_in_j_loop: for (int j = 0; j < CFG::in_channels; ++j){
                             load_in_p_loop: for (int p = 0; p < CFG::kernel_size; ++p) {
                                 load_in_q_loop: for (int q = 0; q < CFG::kernel_size; ++q){
-                                    load_in_zero_check: if ((h + p) % CFG::stride == inpad && (w+q) % CFG::stride == inpad){
-                                        in_block[j][(h - ht) / CFG::stride][(w - wt) / CFG::stride] = in[j * CFG::in_size * CFG::in_size + (h / CFG::stride) * CFG::in_size + (w / CFG::stride)];
+                                    load_in_zero_check: if ((h + ht + p) % CFG::stride == inpad && (w + wt + q) % CFG::stride == inpad){
+                                        in_block[j][h / CFG::stride][w / CFG::stride] = in[j * CFG::in_size * CFG::in_size + ((h + ht) / CFG::stride) * CFG::in_size + ((w + wt) / CFG::stride)];
                                     }   
                                 }
                             }
@@ -74,31 +79,33 @@ void TransposeConv2d_kernel(HWTYPE *in, HWTYPE *bias, HWTYPE *kernel, HWTYPE *ou
                 }
 
                 // perform convolution
-                conv_h_loop: for (int h = ht; h < MIN(ht + CFG::osTile, CFG::out_size); ++h){
-                    conv_w_loop: for (int w = wt; w < MIN(wt + CFG::osTile, CFG::out_size); ++w){
-                        conv_i_loop: for (int i = it; i < MIN(it + CFG::ocTile, CFG::out_channels); ++i){
+                conv_h_loop: for (int h = 0; h < CFG::osTile; ++h){
+                    conv_w_loop: for (int w = 0; w < CFG::osTile; ++w){
+                        conv_i_loop: for (int i = 0; i < CFG::ocTile; ++i){
                             #pragma HLS pipeline II=1
+                            HWTYPE acc = 0;
                             conv_p_loop: for (int p = 0; p < CFG::kernel_size; ++p){
                                 #pragma HLS unroll
                                 conv_q_loop: for (int q = 0; q < CFG::kernel_size; ++q){
                                     #pragma HLS unroll
                                     conv_j_loop: for (int j = 0; j < CFG::in_channels; ++j){
                                         #pragma HLS unroll
-                                        conv_zero_check: if ((h + p) % CFG::stride == inpad && (w+q) % CFG::stride == inpad){
-                                            out_block[i - it][h - ht][w - wt] += weights_block[i - it][j][p][q] * in_block[j][(h - ht) / CFG::stride][(w - wt) / CFG::stride];
+                                        conv_zero_check: if ((h + ht + p) % CFG::stride == inpad && (w + wt + q) % CFG::stride == inpad){
+                                            acc += weights_block[i][j][p][q] * in_block[j][h / CFG::stride][w / CFG::stride];
                                         }                           
                                     }
                                 }
                             }
+                            out_block[i][h][w] = acc;
                         }
                     }
                 } 
 
                 // write output to DRAM
-                write_h_loop: for (int h = ht; h < MIN(ht + CFG::osTile, CFG::out_size); ++h){
-                    write_w_loop: for (int w = wt; w < MIN(wt + CFG::osTile, CFG::out_size); ++w){
-                        write_i_loop: for (int i = it; i < MIN(it + CFG::ocTile, CFG::out_channels); ++i){
-                            out[i * CFG::out_size * CFG::out_size + h * CFG::out_size + w] = out_block[i - it][h - ht][w - wt];
+                write_out_i_loop: for (int i = 0; i < CFG::ocTile; ++i){
+                    write_out_h_loop: for (int h = 0; h < CFG::osTile; ++h){
+                        write_out_w_loop: for (int w = 0; w < CFG::osTile; ++w){
+                            out[(i + it) * CFG::out_size * CFG::out_size + (h + ht) * CFG::out_size + (w + wt)] = out_block[i][h][w];
                         }
                     }
                 }
