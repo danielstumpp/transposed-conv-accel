3768870a30c6b9a5a52a4f78bc60d20020e1cac9
Daniel <dcs98@pitt.edu>
2022-04-26 16:04:21 -0400

    changed indexing in sw src

--- hw/src/TransposeConv2d.cpp
+++ hw/src/TransposeConv2d.cpp
@@ -127,33 +127,34 @@ void TransposeConv2d_arr(DTYPE *in, DTYPE *bias, DTYPE *kernel, DTYPE *out)
             for (int it = 0; it < CFG::out_channels; it += CFG::ocTile){
 
                 // load bias into output block
-                for (int h = ht; h < MIN(ht + CFG::osTile, CFG::out_size); ++h){
-                    for (int w = wt; w < MIN(wt + CFG::osTile, CFG::out_size); ++w){
-                        for (int i = it; i < MIN(it + CFG::ocTile, CFG::out_channels); ++i){
-                            out_block[i - it][h - ht][w - wt] = bias[i];
+                for (int i = 0; i < CFG::ocTile; ++i){
+                    int16_t b = bias[it + i];
+                    for (int h = 0; h < CFG::osTile; ++h){
+                        for (int w = 0; w < CFG::osTile; ++w){
+                            out_block[i][h][w] = b;
                         }
                     }
                 }
 
                 // load kernel weights
-                for (int i = it; i < MIN(it + CFG::ocTile, CFG::out_channels); ++i){
+                for (int i = 0; i < CFG::ocTile; ++i){
                     for (int j = 0; j < CFG::in_channels; ++j){
                         for (int p = 0; p < CFG::kernel_size; ++p){
                             for (int q = 0; q < CFG::kernel_size; ++q){
-                                weights_block[i - it][j][p][q] = kernel[(i * CFG::in_channels * CFG::kernel_size * CFG::kernel_size) + (j * CFG::kernel_size * CFG::kernel_size) + (p * CFG::kernel_size) + q];
+                                weights_block[i][j][p][q] = kernel[((i + it) * CFG::in_channels * CFG::kernel_size * CFG::kernel_size) + (j * CFG::kernel_size * CFG::kernel_size) + (p * CFG::kernel_size) + q];
                             }
                         }
                     }
                 }
 
                 // load input features
-                for (int h = ht; h < MIN(ht + CFG::osTile, CFG::out_size); ++h){
-                    for (int w = wt; w < MIN(wt + CFG::osTile, CFG::out_size); ++w){
+                for (int h = 0; h < CFG::osTile; ++h){
+                    for (int w = 0; w < CFG::osTile; ++w){
                         for (int j = 0; j < CFG::in_channels; ++j){
                             for (int p = 0; p < CFG::kernel_size; ++p) {
                                 for (int q = 0; q < CFG::kernel_size; ++q){
-                                    if ((h + p) % CFG::stride == inpad && (w+q) % CFG::stride == inpad){
-                                        in_block[j][(h - ht) / CFG::stride][(w - wt) / CFG::stride] = in[j * CFG::in_size * CFG::in_size + (h / CFG::stride) * CFG::in_size + (w / CFG::stride)];
+                                    if ((h + ht + p) % CFG::stride == inpad && (w + wt + q) % CFG::stride == inpad){
+                                        in_block[j][h / CFG::stride][w / CFG::stride] = in[j * CFG::in_size * CFG::in_size + ((h + ht)/ CFG::stride) * CFG::in_size + ((w + wt) / CFG::stride)];
                                     }   
                                 }
                             }
@@ -162,15 +163,15 @@ void TransposeConv2d_arr(DTYPE *in, DTYPE *bias, DTYPE *kernel, DTYPE *out)
                 }
 
                 // perform convolution
-                for (int h = ht; h < MIN(ht + CFG::osTile, CFG::out_size); ++h){
-                    for (int w = wt; w < MIN(wt + CFG::osTile, CFG::out_size); ++w){
-                        for (int i = it; i < MIN(it + CFG::ocTile, CFG::out_channels); ++i){
+                for (int h = 0; h < CFG::osTile; ++h){
+                    for (int w = 0; w < CFG::osTile; ++w){
+                        for (int i = 0; i < CFG::ocTile; ++i){
                             for (int j = 0; j < CFG::in_channels; ++j){
                                 for (int p = 0; p < CFG::kernel_size; ++p) {
                                     for (int q = 0; q < CFG::kernel_size; ++q)
                                     {
-                                        if ((h + p) % CFG::stride == inpad && (w+q) % CFG::stride == inpad){
-                                            out_block[i - it][h - ht][w - wt] += weights_block[i - it][j][p][q] * in_block[j][(h - ht) / CFG::stride][(w - wt) / CFG::stride];
+                                        if ((h + ht + p) % CFG::stride == inpad && (w + wt + q) % CFG::stride == inpad){
+                                            out_block[i][h][w] += weights_block[i][j][p][q] * in_block[j][h / CFG::stride][w / CFG::stride];
                                         }                           
                                     }
                                 }
@@ -180,10 +181,11 @@ void TransposeConv2d_arr(DTYPE *in, DTYPE *bias, DTYPE *kernel, DTYPE *out)
                 } 
 
                 // write output to DRAM
-                for (int h = ht; h < MIN(ht + CFG::osTile, CFG::out_size); ++h){
-                    for (int w = wt; w < MIN(wt + CFG::osTile, CFG::out_size); ++w){
-                        for (int i = it; i < MIN(it + CFG::ocTile, CFG::out_channels); ++i){
-                            out[i * CFG::out_size * CFG::out_size + h * CFG::out_size + w] = out_block[i - it][h - ht][w - wt];
+                // write output to DRAM
+                for (int i = 0; i < CFG::ocTile; ++i){
+                    for (int h = 0; h < CFG::osTile; ++h){
+                        for (int w = 0; w < CFG::osTile; ++w){
+                            out[(i + it) * CFG::out_size * CFG::out_size + (h + ht) * CFG::out_size + (w + wt)] = out_block[i][h][w];
                         }
                     }
                 }
